f1815ebac9f8f45f696588a66ff90d59670f58a0
nchinthan <146515522+nchinthan@users.noreply.github.com>
2026-07-03 20:24:09 +0530

    .

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true }
